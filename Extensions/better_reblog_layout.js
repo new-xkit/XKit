f3cb08f89ba601cd7b82f2053382d2946e06905b
Macleod Sawyer <macleodsawyer@users.noreply.github.com>
2015-09-02 18:22:06 -0500

    Added the option to remove avatars

--- Extensions/better_reblog_layout.js
+++ Extensions/better_reblog_layout.js
@@ -10,17 +10,23 @@ XKit.extensions.better_reblog_layout = new Object({
 	running: false,
 	
 	preferences: {
-		"sep0": {
-			text: 'Options',
-			type: 'separator'
-		},
-		"normal": {
+    	'sep0': {
+          text: 'Options',
+          type: 'separator'
+        },
+        
+	    "normal": {
 			text: "Move reblog content to the right (under the username, not avatar)",
 			default: true,
 			value: true
-		},
+		}, 
 		"add_border": {
 			text: "Add border to the left",
+			default: false,
+			value: false
+		},
+		"remove_user_names": {
+			text: "Remove usernames",
 			default: false,
 			value: false
 		},
@@ -34,11 +40,15 @@ XKit.extensions.better_reblog_layout = new Object({
 		this.running = true;
 		
 		if (XKit.extensions.better_reblog_layout.preferences.normal.value === true) {
-	       XKit.tools.add_css(" .reblog-content {margin-left:35px;", "better_reblog_layout_normal");
+	       XKit.tools.add_css(" .reblog-content {margin-left:35px;}", "better_reblog_layout_normal");
 	    }
 	    
 	    if (XKit.extensions.better_reblog_layout.preferences.add_border.value === true) {
 	       XKit.tools.add_css(" .reblog-list-item .reblog-content {border-left: 1px solid #E7E7E7;padding-left: 10px;}", "better_reblog_layout_add_border");
+	    }
+	    
+	    if (XKit.extensions.better_reblog_layout.preferences.remove_user_names.value === true) {
+	       XKit.tools.add_css(" .reblog-tumblelog-name {display:none;} .reblog-list-item .reblog-header {margin-bottom: 0;} .reblog-content {margin-left:35px;}", "better_reblog_layout_remove_user_names");
 	    }
 	    
 		
@@ -50,6 +60,7 @@ XKit.extensions.better_reblog_layout = new Object({
 		
 		XKit.tools.remove_css("better_reblog_layout_normal");
 		XKit.tools.remove_css("better_reblog_layout_add_border");
+		XKit.tools.remove_css("better_reblog_layout_remove_user_names");
 	}
 
 });
