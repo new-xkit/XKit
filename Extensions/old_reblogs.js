e423f6d759d2b581404579bf5775aabeca8c93a7
ewenig <eli@csh.rit.edu>
2015-09-03 00:40:55 -0700

    fixed old-reblog-style formatting on the Dashboard

--- Extensions/old_reblogs.js
+++ Extensions/old_reblogs.js
@@ -1,6 +1,6 @@
 //* TITLE Restore Old Reblogs **//
 //* VERSION 1.0.1 **//
-//* DESCRIPTION	Restores the old reblogging style **//
+//* DESCRIPTION	Restores the old reblogging style on the Dashboard **//
 //* DEVELOPER eli <eli@csh.rit.edu> (forked from Editable Reblogs by dlmarquis) **//
 //* FRAME false **//
 //* BETA true **//
@@ -12,59 +12,52 @@ XKit.extensions.old_reblogs = new Object({
 	run: function() {
 		this.running = true;
 
-		XKit.interface.post_window_listener.add("old_reblogs", XKit.extensions.old_reblogs.post_window);
-		XKit.tools.add_css(".btn-toggle-reblog {display: none; }", "old_reblogs_remove_content_chain");
-		XKit.tools.add_css(".xkit-old-reblogs-done {display: none; }", "old_reblogs_remove_content_chain");
+		XKit.post_listener.add("old_reblogs", XKit.extensions.old_reblogs.main);
+		XKit.extensions.old_reblogs.main();
 	},
 
-	post_window: function() {
-		var reblog_chain = $(".reblog-list");
-		if (reblog_chain.length <= 0) {
+	main: function() {
+		var post_list = $(".post_content_inner");
+		if (post_list.length <= 0) {
 			return;
 		}
 
-		// Guard against double evaluation by marking the tree as processed
-		var processed_class = 'xkit-old-reblogs-done';
-		if (reblog_chain.hasClass(processed_class)) {
-			return;
-		}
-		if (reblog_chain.attr('data-subview') === "reblogCollectionView") {
+		post_list.each(function() {
+			var reblog_chain = $(this).find("div.reblog-list");
+			var reblog_text = $(this).find("div.contributed-content > div.reblog-content");
+
+			// Guard against double evaluation by marking the tree as processed
+			var processed_class = 'xkit-old-reblogs-done';
+			if (reblog_chain.hasClass(processed_class)) {
+				return;
+			}
 			reblog_chain.addClass(processed_class);
-		}
 
-		// re-enable display of the reblog chain when the form is closed
-		$(".post-form--close").click(function({
-			reblog_chain.delClass(processed_class);
+			var users_chain = reblog_chain.find('a.reblog-tumblelog-name');
+			var content_chain = reblog_chain.find('div.reblog-content');
+			var reblog_content = '<div class="reblog-list-item"><div class="reblog-content">';
+
+			$(users_chain.get().reverse()).each(function() {
+				reblog_content += '<p><a class="tumblr_blog" href="' + $(this).attr('href') + '">' + $(this).html() + '</a>:<blockquote>';
+			});
+			content_chain.each(function() {
+				reblog_content += $(this).html();
+				reblog_content += '</blockquote></p>';
+			});
+			if (typeof(reblog_text.html()) !== "undefined") {
+				// add the user's reblog text to the bottom
+				reblog_content += '<p>';
+				reblog_content += reblog_text.html();
+				reblog_content += '</p>';
+			}
+			reblog_content += '</div></div>';
+
+			reblog_chain.html(reblog_content);
 		});
-
-		// Convert all of the user links to have class tumblr_blog
-		var users_chain = reblog_chain.find('div.reblog-tumblelog-name');
-		var content_chain = reblog_chain.find('div.reblog-content');
-
-		var reblog_content = "";
-
-		if (top_quote_link.length > 0) {
-			$(top_quote_link[0]).addClass('tumblr_blog');
-		}
-		
-		$(users_chain.get().reverse()).each(function() {
-			reblog_content += '<p><a class="tumblr_blog" href="' + $(this).attr('href') + '">' + $(this).html() + '</a><blockquote>';
-		});
-		content_chain.each(function() {
-			reblog_content += $(this).html();
-			reblog_content += '</blockquote></p>';
-		});
-
-		console.log(reblog_content);
-		var old_content = XKit.interface.post_window.get_content_html();
-		XKit.interface.post_window.set_content_html(reblog_content + old_content);
-
-		$(".btn-remove-tree").click();
 	},
 
 	destroy: function() {
 		this.running = false;
-		XKit.tools.remove_css("old_reblogs_remove_content_chain");
 		XKit.interface.post_window_listener.remove("old_reblogs");
 	}
 });
