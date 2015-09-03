a4f180a64d03f70d8d14a9b5dc81d62be11fb408
ewenig <eli@csh.rit.edu>
2015-09-03 01:15:36 -0700

    add cleanup + fixes

--- Extensions/old_reblogs.js
+++ Extensions/old_reblogs.js
@@ -13,29 +13,38 @@ XKit.extensions.old_reblogs = new Object({
 		this.running = true;
 
 		XKit.post_listener.add("old_reblogs", XKit.extensions.old_reblogs.main);
+		XKit.tools.add_css('.xkit-old-reblogs-done div.reblog-list { display: none }','xkit_old_reblogs_remove_reblog_list');
+		XKit.tools.add_css('.xkit-old-reblogs-done div.contributed-content { display: none }','xkit_old_reblogs_remove_reblog_content');
 		XKit.extensions.old_reblogs.main();
 	},
 
 	main: function() {
-		var post_list = $(".post_content_inner");
-		if (post_list.length <= 0) {
+		var posts = XKit.interface.get_posts('xkit-old-reblogs-done', false);
+		window.console.log(posts);
+		if (posts.length <= 0) {
+			// no posts
 			return;
 		}
 
-		post_list.each(function() {
+		$(posts).each(function() {
+			// get the reblog-list for the post
 			var reblog_chain = $(this).find("div.reblog-list");
+			// get the text that the user added to the post (might not exist but it's
+			// sanity checked later)
 			var reblog_text = $(this).find("div.contributed-content > div.reblog-content");
 
 			// Guard against double evaluation by marking the tree as processed
 			var processed_class = 'xkit-old-reblogs-done';
-			if (reblog_chain.hasClass(processed_class)) {
+			if ($(this).hasClass(processed_class)) {
 				return;
 			}
-			reblog_chain.addClass(processed_class);
+			$(this).addClass(processed_class);
 
 			var users_chain = reblog_chain.find('a.reblog-tumblelog-name');
 			var content_chain = reblog_chain.find('div.reblog-content');
-			var reblog_content = '<div class="reblog-list-item"><div class="reblog-content">';
+
+			// reconstitute the text post
+			var reblog_content = '<div class="reblog-list-item xkit-old-reblogs-new"><div class="reblog-content">';
 
 			$(users_chain.get().reverse()).each(function() {
 				reblog_content += '<p><a class="tumblr_blog" href="' + $(this).attr('href') + '">' + $(this).html() + '</a>:<blockquote>';
@@ -52,12 +61,20 @@ XKit.extensions.old_reblogs = new Object({
 			}
 			reblog_content += '</div></div>';
 
-			reblog_chain.html(reblog_content);
+			// add the newly formatted text to the post body
+			$(this).find('.post_content_inner').append(reblog_content);
 		});
 	},
 
 	destroy: function() {
+		$("div.xkit-old-reblogs-new").remove();
+		$('.xkit-old-reblogs-done').each(function() {
+			$(this).removeClass('xkit-old-reblogs-done');
+		});
+
 		this.running = false;
+		XKit.tools.remove_css("xkit_old_reblogs_remove_reblog_list");
+		XKit.tools.remove_css("xkit_old_reblogs_remove_reblog_content");
 		XKit.interface.post_window_listener.remove("old_reblogs");
 	}
 });
