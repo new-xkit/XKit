ce2812d6b2ba223e9ed7edcf4f63588091202e98
Jon <jon@palantetech.coop>
2015-11-10 22:09:54 -0500

    blacklist.js - preserve href links - catch URLs when
    ...href isn't the last element of the tag.

--- Extensions/blacklist.js
+++ Extensions/blacklist.js
@@ -553,7 +553,7 @@ XKit.extensions.blacklist = new Object({
 				m_content = m_content.toLowerCase();
 				
 			    // Preserve href links.
-			    m_content = m_content.replace(/<a\s+(?:[^>]*?\s+)?href="([^"]*)">/gm, "$1" + ' ');
+			    m_content = m_content.replace(/<a\s+(?:[^>]*?\s+)?href="([^"]*)".*?>/gm, "$1" + ' ');
 				// Strip HTML tags.
 				m_content = m_content.replace(/<(?:.|\n)*?>/gm, ' ');
 
