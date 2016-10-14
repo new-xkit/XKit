016d3472c80c9d1c558dcc4fbc4e9fa352b0c41b
BlackjackKent <tumblrthreadtracker@gmail.com>
2016-10-13 19:51:22 -0500

    Adding preferences screen

--- Extensions/draft_saver.js
+++ Extensions/draft_saver.js
@@ -13,6 +13,7 @@ XKit.extensions.draft_saver = new Object({
 
 	run: function(){
 		this.running = true;
+		XKit.tools.init_css("draft_saver");
 		var number_of_saves = XKit.extensions.draft_saver.preferences.number_of_saves.value;
 		var time_between_saves = XKit.extensions.draft_saver.preferences.number_of_saves.value;
 		this.interval = setInterval(this.save_current_content, time_between_saves * 1000);
@@ -42,6 +43,10 @@ XKit.extensions.draft_saver = new Object({
 				"20 saves", 30
 			]
 		},
+		"sep1": {
+			text: "Saved Drafts",
+			type: "separator"
+		}
 	},
 
 	save_current_content: function() {
@@ -93,8 +98,81 @@ XKit.extensions.draft_saver = new Object({
 		return false;
 	},
 
+	cpanel: function(div) {
+		var drafts = XKit.storage.get_all('draft_saver');
+		console.log(drafts);
+		var html = '';
+		for (var key in drafts) {
+		    if (drafts.hasOwnProperty(key)) {
+		    	var date = XKit.extensions.draft_saver.build_time_from_key(key);
+		    	var shortContent = $(drafts[key].value).text();
+		    	if (shortContent.length > 100) {
+		    		shortContent = shortContent.substr(0, 100) + '...';
+		    	}
+		    	html +=	"<div class=\"xkit-draft-saver-cp\">" +
+					"<div class=\"xkit-draft-title\">" + date + " <a class='xkit-draft-expand'>Expand</a></div>" +
+					"<div class='xkit-draft-content'>" + shortContent + "</div>" +
+					"</div>";
+		        console.log(key);
+		        console.log(drafts[key]);
+		        console.log(XKit.extensions.draft_saver.build_time_from_key(key));
+		    }
+		}
+		div.append(html);
+		$(".xkit-draft-saver-cp").click(function(e) {
+			console.log(e)
+			//XKit.extensions.draft_saver.add_bundle_ui();
+		});
+	},
+
+	build_time_from_key: function(key) {
+		var a = new Date(parseInt(key));
+		var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+		var year = a.getFullYear();
+		var month = months[a.getMonth()];
+		var date = a.getDate();
+		var hour = a.getHours();
+		var min = a.getMinutes();
+		var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min ;
+		return time;
+	},
+
 	destroy: function(){
 		this.running = false;
+		$(".xkit-draft-saver-cp").unbind("click");
 		clearInterval(this.interval);
+	},
+	add_bundle_ui: function(e) {
+
+		XKit.window.show("Create new bundle","<b>Bundle Title</b><input type=\"text\" maxlength=\"40\" placeholder=\"eg: Doctor Who\" class=\"xkit-textbox\" id=\"xkit-quick-tags-add-title\"><b>Bundled Tags, comma separated</b><input type=\"text\" maxlength=\"250\" placeholder=\"eg: Doctor Who, Dr. Who, Non-Medical Tv Show Doctor\" class=\"xkit-textbox\" id=\"xkit-quick-tags-add-tags\">You have <b>" + remaining + "</b> bundle slots left.","question","<div class=\"xkit-button default\" id=\"xkit-quick-tags-create-bundle\">Create Bundle</div><div class=\"xkit-button\" id=\"xkit-close-message\">Cancel</div>");
+
+		$("#xkit-quick-tags-create-bundle").click(function() {
+
+			var title = $("#xkit-quick-tags-add-title").val();
+			var tags = $("#xkit-quick-tags-add-tags").val();
+
+			if ($.trim(title) === "") {
+				alert("Please enter a title for your bundle.");
+				return;
+			}
+
+			if ($.trim(tags) === "") {
+				alert("Please enter the tags for your bundle.");
+				return;
+			}
+
+			var m_object = {};
+			m_object.title = title;
+			m_object.tags = tags;
+			XKit.extensions.quick_tags.tag_array.push(m_object);
+
+			XKit.storage.set("quick_tags","user_tags", JSON.stringify(XKit.extensions.quick_tags.tag_array));
+
+			XKit.window.close();
+			XKit.extensions.quick_tags.cpanel(m_div);
+			XKit.extensions.xkit_preferences.restart_extension("quick_tags");
+
+		});
+
 	}
 });
