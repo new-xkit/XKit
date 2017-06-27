ab61b6b7b51c83bd05cf781844631cea3190cc20
April Sylph <aprilsylph@outlook.com>
2017-06-27 20:50:04 +0100

    Defeated a tumblr update

--- Extensions/tweaks.js
+++ Extensions/tweaks.js
@@ -320,7 +320,7 @@ XKit.extensions.tweaks = new Object({
 			".ui_notes .activity-notification .activity-notification__activity .activity-notification__activity_message{ display: block; }" +
 			".ui_notes .activity-notification .activity-notification__activity, .ui_notes .activity-notification .activity-notification__activity .activity-notification__activity_message.conversational{ transform: translate(-10px) }" +
 			".xkit-activity-plus-timestamp{ transform: translate(-53px, 6px) }" +
-			".ui_notes .activity-notification .activity-notification__icon{	padding: 0; }" +
+			".ui_notes .activity-notification .activity-notification__icon{	padding: 0; min-width: 25px; }" +
 			".ui_notes .activity-notification .activity-notification__activity .activity-notification__activity_message .activity-notification__activity_response{ padding: 10px; }" +
 			".ui_notes .activity-notification .activity-notification__avatar .ui_avatar{ margin: 0; }" +
 			".ui_notes .activity-notification .activity-notification__icon .ui_post_badge{ width: 25px; height: 25px; }" +
