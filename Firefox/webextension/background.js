d26566a904ef24a165fb33ebd2389591d4689411
James Hobin <hobinjk@gmail.com>
2017-08-08 22:39:44 -0700

    Asynchronously migrate extension data

--- Firefox/webextension/background.js
+++ Firefox/webextension/background.js
@@ -4,18 +4,17 @@
 
 // Ask to the legacy part to dump the needed data and send it back
 // to the background page...
-var port = browser.runtime.connect({name: "sync-legacy-addon-data"});
-port.onMessage.addListener((msg) => {
-	if (msg) {
-		browser.storage.local.get('isMigrated').then(function(item) {
-			if (item.isMigrated) {
-				console.warn('Skipping migration');
-				return;
-			}
-			// The corresponding code in XKit is GM_setValue(name, value) -> set({name: value})
-			msg.isMigrated = true;
-			console.log('Migrating pref storage', msg);
-			browser.storage.local.set(msg);
-		});
+browser.storage.local.get('isProperlyMigrated').then(function(item) {
+	if (item.isProperlyMigrated) {
+		console.warn('Skipping migration', item);
+		return;
 	}
+
+	var port = browser.runtime.connect({name: "sync-legacy-addon-data"});
+	port.onMessage.addListener((msg) => {
+		if (msg) {
+			// The corresponding code in XKit is GM_setValue(name, value) -> set({name: value})
+			browser.storage.local.set(msg);
+		}
+	});
 });
