252f052629b54ed3c59df6297603b5c13e07a3c9
James Hobin <hobinjk@gmail.com>
2017-08-09 22:29:07 -0700

    Skip update if current value is longer

--- Firefox/webextension/background.js
+++ Firefox/webextension/background.js
@@ -1,4 +1,4 @@
-/* global browser */
+/* global browser, Promise */
 
 // From the MDN WebExtensions hybrid addon example
 
@@ -11,10 +11,33 @@ browser.storage.local.get('isProperlyMigrated').then(function(item) {
 	}
 
 	var port = browser.runtime.connect({name: "sync-legacy-addon-data"});
-	port.onMessage.addListener((msg) => {
-		if (msg) {
-			// The corresponding code in XKit is GM_setValue(name, value) -> set({name: value})
-			browser.storage.local.set(msg);
+	function onMessage(msg) {
+		if (!msg) {
+			return;
 		}
-	});
+		let setPromises = Object.keys(msg).map(function(key) {
+			return browser.storage.local.get({key: null}).then(function(msgItem) {
+				if (typeof(msgItem[key]) === 'string') {
+					if (msgItem[key].length > msg[key].length) {
+						// Do not update the storage
+						return Promise.resolve();
+					}
+				}
+				// The corresponding code in XKit is GM_setValue(name, value) -> set({name: value})
+				return browser.storage.local.set(msg);
+			});
+		});
+		Promise.all(setPromises).catch(function(err) {
+			let errorMessage = "XKit failed to migrate your preferences to the new storage system. The migration reported the following error messag: " + err + ". Please restart your browser to try again.";
+			if (typeof(XKit) === 'undefined') {
+				alert(errorMessage);
+			} else {
+				XKit.window.show("Storage migration in progress", errorMessage, "error",  "<div class=\"xkit-button default\">OK</div>");
+			}
+			browser.storage.local.set({isProperlyMigrated: false});
+			port.onMessage.removeListener(onMessage);
+		});
+	}
+
+	port.onMessage.addListener(onMessage);
 });
