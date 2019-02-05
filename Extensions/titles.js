c9a79d1f01d6e2ec6fc1a6185d6c79998392331b
Rebecca Turner <637275@gmail.com>
2019-02-04 22:13:41 -0500

    Changes requested by nightpool

--- Extensions/titles.js
+++ Extensions/titles.js
@@ -12,90 +12,72 @@ XKit.extensions.titles = new Object({
 	 * an array of url components (like ['blog', 'xkit'] for '/blog/xkit') and
 	 * returns the new page title
 	 */
-	titleFuncs: {
-		blog: url => {
-			if (url.length > 2) {
-				if (url[2] == "new") {
-					// new post
-					return "Post to " + url[1];
-				} else if (url[2] == "review") {
-					return url[1] + "’s flagged posts";
-				} else if (url[2] != "delete") {
-					// e.g. drafts, queue
-					return url[1] + "’s " + url[2];
-				} else {
-					return "Delete " + url[1];
-				}
+	titleForPath: {
+		blog: path => {
+			if (path.length > 2) {
+				const [blogName, route] = path;
+				return {
+					"new": `Post to ${blogName}`,
+					review: `${blogName}’s flagged posts`,
+					delete: `Delete ${blogName}`
+				}[route] || `${blogName}’s ${route}`;
 			}
 			// a plain blog page
-			return url[1];
+			return path[1];
 		},
 
-		explore: url => "Explore: " + {
+		explore: path => "Explore: " + {
 			"recommended-for-you": "Recommended for you",
-			"trending":            "Trending",
-			"staff-picks":         "Staff picks",
-			"text":                "Text posts",
-			"photos":              "Photo posts",
-			"gifs":                "GIF posts",
-			"quotes":              "Quote posts",
-			"links":               "Link posts",
-			"chats":               "Chat posts",
-			"audio":               "Audio posts",
-			"video":               "Video posts",
-			"asks":                "Ask posts"
-		}[url[1]],
+			trending: "Trending",
+			"staff-picks": "Staff picks",
+			text: "Text posts",
+			photos: "Photo posts",
+			gifs: "GIF posts",
+			quotes: "Quote posts",
+			links: "Link posts",
+			chats: "Chat posts",
+			audio: "Audio posts",
+			video: "Video posts",
+			asks: "Ask posts"
+		}[path[1]],
 
-		dashboard: url => "Dashboard",
-		inbox: url => "Inbox",
-		tagged: url => "#" + url[1],
-		search: url => "Search: " + url[1],
-		likes: url => "Likes",
-		following: url => "Following",
+		dashboard: path => "Dashboard",
+		inbox: path => "Inbox",
+		tagged: path => `#${path[1]}`,
+		search: path => `Search: ${path[1]}`,
+		likes: path => "Likes",
+		following: path => "Following",
 
-		settings: url => url[1] == "blog"
-			? "Settings for " + url[2]
-			: document.title,
-
-		"new": url => url[1] == "blog"
+		"new": path => path[1] == "blog"
 			? "Hoard a URL"
-			: "Post to " + XKit.interface.post_window.blog(),
+			: `Post to ${XKit.interface.post_window.blog()}`,
 
-		reblog: url => "Reblog from " + XKit.interface.post_window.reblogging_from(),
-		"mega-editor": url => "Mass editing " + url[1]
+		reblog: path => `Reblog from ${XKit.interface.post_window.reblogging_from()}`,
+		"mega-editor": path => `Mass editing ${path[1]}`
 	},
-
-	// new page title from an array of directory names
-	getTitle: function(url) {
-		return this.titleFuncs[url[0]](url);
-	},
-
-	urlComponents: () => new window.URL(document.URL).pathname
-			.split("/").filter(component => component),
 
 	// trims text after a `|` in a string; used for titles like
 	// "Dashboard | Tumblr"
-	trimPipe: txt => {
-		let pipe = txt.indexOf("|");
-		if (pipe != -1) {
-			return txt.substring(0, pipe);
+	trimTitle: title => title.split("|")[0].trim(),
+
+	// Gets a new page title from an array of directory names. This actually
+	// has to be a function() and not a lambda because it uses `this`.
+	getTitle: function(pathComponents) {
+		let fn = this.titleFuncs[pathComponents[0]];
+		if (fn) {
+			return fn(pathComponents);
+		} else {
+			// unrecognized path; clean up title a bit anyways
+			return this.trimTitle(document.title);
 		}
-		return txt.trim();
 	},
+
+	pathComponents: () => document.location.pathname
+			.split("/").filter(component => component),
 
 	run: function() {
 		this.running = true;
-		document.title = this.trimPipe(document.title);
-		// array of non-empty directory names, like ['blog', 'new-xkit']
-		let url = this.urlComponents();
-
-		window.addEventListener("hashchange", e => {
-			if (this.running) {
-				document.title = this.getTitle(url);
-			}
-		});
-
-		document.title = this.getTitle(url);
+		document.title = this.getTitle(this.pathComponents());
 	},
 
 	destroy: function() {
