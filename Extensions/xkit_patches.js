77e944d2413f76d3d8b89d1322b6e07aa7f59739
cyle <cyle@tumblr.com>
2019-11-18 11:28:54 -0500

    include xkit version as a header

--- Extensions/xkit_patches.js
+++ Extensions/xkit_patches.js
@@ -389,7 +389,8 @@ XKit.extensions.xkit_patches = new Object({
 
 				const standard_headers = {
 					"X-Requested-With": "XMLHttpRequest",
-					"X-Tumblr-Form-Key": XKit.interface.form_key()
+					"X-Tumblr-Form-Key": XKit.interface.form_key(),
+					"X-XKit-Version": XKit.version
 				};
 
 				if (details.headers === undefined) {
