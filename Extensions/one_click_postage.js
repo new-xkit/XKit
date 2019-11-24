02690b3dea78790cefd9591de7c6939dd9c6b657
April Sylph <april@sylph.tech>
2019-11-24 21:09:27 +0000

    [One-Click Postage] Always retain selected blog

--- Extensions/one_click_postage.js
+++ Extensions/one_click_postage.js
@@ -1,5 +1,5 @@
 //* TITLE One-Click Postage **//
-//* VERSION 4.4.6 **//
+//* VERSION 4.4.7 **//
 //* DESCRIPTION Lets you easily reblog, draft and queue posts **//
 //* DEVELOPER new-xkit **//
 //* FRAME false **//
@@ -606,29 +606,26 @@ XKit.extensions.one_click_postage = new Object({
 			$("#x1cpostage_box").removeClass("xkit_x1cpostage_queue_press");
 		});
 
-		var m_blogselector_html = "";
-
 		XKit.extensions.one_click_postage.blogs_list = m_blogs;
 		XKit.extensions.one_click_postage.default_blog_id = m_blogs[0];
 
 		if (this.preferences.show_blog_selector.value) {
-			for (var i = 0; i < m_blogs.length; i++) {
-				if (m_blogs[i] !== "") {
-					var extra_cls = "";
-					if (this.preferences.default_blog.value !== "") {
-						if (this.preferences.default_blog.value === m_blogs[i]) {
-							extra_cls = "selected";
-						}
-					}
-					m_blogselector_html = m_blogselector_html + "<option " + extra_cls + " value=\"" +
-							m_blogs[i] + "\">" + m_blogs[i] + "</option>";
-				}
+			let m_blogselector_html = '<select id="x1cpostage_blog">';
+
+			for (let blog of m_blogs) {
+				m_blogselector_html += `<option value="${blog}">${blog}</option>`;
 			}
-			m_blogselector_html = "<select id=\"x1cpostage_blog\">" + m_blogselector_html + "</select>";
+
+			m_blogselector_html += '</select>';
+
 			if (this.preferences.show_reverse_ui.value) {
 				$("#x1cpostage_reblog").before(m_blogselector_html);
 			} else {
 				$("#x1cpostage_caption").before(m_blogselector_html);
+			}
+
+			if (m_blogs.includes(this.preferences.default_blog.value)) {
+				$("#x1cpostage_blog").val(this.preferences.default_blog.value);
 			}
 		}
 
@@ -998,11 +995,6 @@ XKit.extensions.one_click_postage = new Object({
 		$("#x1cpostage_tags").css("border-top", "0px");
 		$("#x1cpostage_caption").css("height", XKit.extensions.one_click_postage.caption_height + "px");
 
-		if (this.preferences.show_blog_selector.value && this.preferences.default_blog.value !== "") {
-			$("#x1cpostage_blog option:selected").prop("selected", false);
-			$("#x1cpostage_blog option[value='" + this.preferences.default_blog.value + "']").prop("selected", true);
-		}
-
 		$(obj).attr('title', '');
 
 		// Call Auto Tagger for tags. Will be "" if auto_tagger is disabled
@@ -1348,7 +1340,7 @@ XKit.extensions.one_click_postage = new Object({
 					}
 
 					$(m_button).removeClass("xkit-one-click-reblog-working");
-					
+
 					if (responseData.message === "" || typeof responseData.message === "undefined") {
 						// No message
 					} else {
