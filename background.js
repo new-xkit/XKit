b7e16d31bfd13bf950589e6448eacbd387b3b366
xenticore <pearl@bleatbox.net>
2020-07-19 14:29:45 -0400

    Delete unnecessary polyfill

--- background.js
+++ background.js
@@ -1,3 +1,13 @@
+/* globals chrome, browser, msBrowser */
+
+if (typeof(browser) === 'undefined') {
+	if (typeof(chrome) !== 'undefined') {
+		browser = chrome; // eslint-disable-line no-global-assign
+	} else if (typeof(msBrowser) !== 'undefined') {
+		browser = msBrowser; // eslint-disable-line no-global-assign
+	}
+}
+
 function openEditor(message) {
 	if(message.action === 'createeditor') {
 		let createData = {
