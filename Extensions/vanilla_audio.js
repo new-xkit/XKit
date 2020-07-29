deba6ae9e6af4f73accb593ea17885ad96392edd
xenticore <pearl@bleatbox.net>
2020-07-28 23:57:18 -0400

    Make compatible with new dash

--- Extensions/vanilla_audio.js
+++ Extensions/vanilla_audio.js
@@ -1,5 +1,5 @@
 //* TITLE Vanilla Audio **//
-//* VERSION 0.2.1 **//
+//* VERSION 0.3.0 **//
 //* DESCRIPTION	Adds an alternative audio player to audio posts. **//
 //* DETAILS This extension adds a native HTML5 audio player to every audio post, with an option for the default volume and the ability to loop the audio. **//
 //* DEVELOPER akunohomu **//
@@ -26,11 +26,34 @@ XKit.extensions.vanilla_audio = new Object({
 		}
 	},
 
-	run: function() {
+	run: async function() {
 		"use strict";
 		this.running = true;
+
+		await XKit.css_map.getCssMap();
+
+		if (XKit.page.react) {
+			XKit.tools.add_css(".xkit-audio-added { padding: 0 16px; } .xkit-audio-player { display: block !important; margin: auto; } }", "vanilla_audio");
+			XKit.post_listener.add("vanilla_audio", this.add_audio_react);
+			this.add_audio_react();
+			return;
+		}
+
 		XKit.post_listener.add("vanilla_audio", this.add_audio);
 		this.add_audio();
+	},
+
+	add_audio_react: function() {
+		let audiocontainer_classname = XKit.css_map.keyToClasses('nativePlayer')[0];
+		$(`.${audiocontainer_classname}`).not(".xkit-audio-added").each(function() {
+			var $this = $(this);
+			$this.addClass("xkit-audio-added");
+			$this.children().css("display", "none");
+			var hidden_player = $this.find("audio");
+			var xkit_player = $("<audio/>", { "controls": "controls", "class": "xkit-audio-player" }).appendTo($this);
+			xkit_player.prop("volume", XKit.extensions.vanilla_audio.preferences.default_volume.value);
+			hidden_player.find("source").clone().appendTo(xkit_player);
+		});
 	},
 
 	add_audio: function() {
@@ -52,6 +75,9 @@ XKit.extensions.vanilla_audio = new Object({
 	destroy: function() {
 		"use strict";
 		XKit.post_listener.remove("vanilla_audio");
+		if (XKit.page.react) {
+			$(".xkit-audio-added").children().css("display", "flex");
+		}
 		$(".xkit-audio-added").removeClass("xkit-audio-added");
 		$(".xkit-audio-player").remove();
 		this.running = false;
