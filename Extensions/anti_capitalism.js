08138e27968547952f491e21b989f00856b42163
nightpool <nightpool@users.noreply.github.com>
2020-08-04 13:20:16 -0400

    Update anti_capitalism.js

--- Extensions/anti_capitalism.js
+++ Extensions/anti_capitalism.js
@@ -1,5 +1,5 @@
 //* TITLE Anti-Capitalism **//
-//* VERSION 1.5.7 **//
+//* VERSION 1.6.1 **//
 //* DESCRIPTION Removes sponsored posts, vendor buttons, and other nonsense that wants your money. **//
 //* DEVELOPER new-xkit **//
 //* FRAME false **//
