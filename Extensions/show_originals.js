8a772ed3d51c3fec413edc483308bd6f55af4904
Marcus <marcustyphoon@gmail.com>
2020-08-09 17:21:34 -0700

    improved CSS color consistency

--- Extensions/show_originals.js
+++ Extensions/show_originals.js
@@ -68,18 +68,18 @@ XKit.extensions.show_originals = new Object({
 
 		if (XKit.page.react) {
 
-			const automatic_color = 'var(--blog-contrasting-title-color,var(--rgb-white-on-dark))';
+			const automatic_color = 'var(--blog-contrasting-title-color,var(--transparent-white-65))';
 
 			XKit.tools.add_css(`
 				.showoriginals-hidden {
-					opacity: 0.5;
+					opacity: 0.65;
 					margin-bottom:8px;
 					transform: translateY(-6px);
 				}
 				.showoriginals-hidden-note {
 					height: 30px !important;
 					line-height: 30px !important;
-					color: rgba(${automatic_color}, 0.8);
+					color: ${automatic_color};
 					padding: 0;
 					margin: 0;
 					padding-left: 15px;
@@ -156,7 +156,7 @@ XKit.extensions.show_originals = new Object({
 				$this.prepend('<div class="showoriginals-hidden-note">Hidden by Show Originals</div>');
 
 			} else {
-				const reblogicon = '<svg viewBox="0 -7.5 12.3 28" width="25" height="30" fill="var(--blog-contrasting-title-color,var(--white-on-dark))" fill-opacity="0.5"><path d="M9.2.2C8.7-.2 8 .2 8 .8v1.1H3.1c-2 0-3.1 1-3.1 2.6v1.9c0 .5.4.9.9.9.1 0 .2 0 .3-.1.3-.1.6-.5.6-.8V5.2c0-1.4.3-1.5 1.3-1.5H8v1.1c0 .6.7 1 1.2.6l3.1-2.6L9.2.2zM12 7.4c0-.5-.4-.9-.9-.9s-.9.4-.9.9v1.2c0 1.4-.3 1.5-1.3 1.5H4.3V9c0-.6-.7-.9-1.2-.5L0 11l3.1 2.6c.5.4 1.2.1 1.2-.5v-1.2h4.6c2 0 3.1-1 3.1-2.6V7.4z"></path></svg>';
+				const reblogicon = '<svg viewBox="0 -7.5 12.3 28" width="25" height="30" fill="var(--blog-contrasting-title-color,var(--transparent-white-65))" fill-opacity="0.65"><path d="M9.2.2C8.7-.2 8 .2 8 .8v1.1H3.1c-2 0-3.1 1-3.1 2.6v1.9c0 .5.4.9.9.9.1 0 .2 0 .3-.1.3-.1.6-.5.6-.8V5.2c0-1.4.3-1.5 1.3-1.5H8v1.1c0 .6.7 1 1.2.6l3.1-2.6L9.2.2zM12 7.4c0-.5-.4-.9-.9-.9s-.9.4-.9.9v1.2c0 1.4-.3 1.5-1.3 1.5H4.3V9c0-.6-.7-.9-1.2-.5L0 11l3.1 2.6c.5.4 1.2.1 1.2-.5v-1.2h4.6c2 0 3.1-1 3.1-2.6V7.4z"></path></svg>';
 
 				var note_text = '';
 				if (hide_posts_makelink.value) {
