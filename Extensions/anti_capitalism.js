5d10a786762f996720c24f36d4b30abf60f616ab
Marcus <marcustyphoon@gmail.com>
2020-08-13 01:40:34 -0700

    Don't run on "grid/masonry" tagged page

--- Extensions/anti_capitalism.js
+++ Extensions/anti_capitalism.js
@@ -1,5 +1,5 @@
 //* TITLE Anti-Capitalism **//
-//* VERSION 1.6.2 **//
+//* VERSION 1.6.3 **//
 //* DESCRIPTION Removes sponsored posts, vendor buttons, and other nonsense that wants your money. **//
 //* DEVELOPER new-xkit **//
 //* FRAME false **//
@@ -61,9 +61,12 @@ XKit.extensions.anti_capitalism = new Object({
 		if (XKit.page.react) {
 			await XKit.css_map.getCssMap();
 
+			//exit if we're on a grid page to avoid breaking anything
+			if ($(XKit.css_map.keyToCss("masonry")).length) { return; }
+
 			if (this.preferences.sponsored_posts.value) {
 				const selector = XKit.css_map.keyToClasses("listTimelineObject").map(css => `.${css}:not([data-id])`).join(",");
-				XKit.tools.add_css(`${selector} {height: 0; margin: 0; overflow: hidden;}", "anti_capitalism`);
+				XKit.tools.add_css(`${selector} {height: 0; margin: 0; overflow: hidden;}`, "anti_capitalism");
 			}
 
 			if (this.preferences.sidebar_ad.value) {
