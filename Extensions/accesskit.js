9186e050c6d8e0fd1dd27cdf58c9501373dc9975
Marcus <marcustyphoon@gmail.com>
2020-08-19 12:15:02 -0700

    version bump 🤦
    I have a "before you PR" check list now so hoooopefully this never happens again

--- Extensions/accesskit.js
+++ Extensions/accesskit.js
@@ -1,5 +1,5 @@
 //* TITLE AccessKit **//
-//* VERSION 2.0.0 **//
+//* VERSION 2.0.1 **//
 //* DESCRIPTION Accessibility tools for Tumblr **//
 //* DETAILS Provides accessibility tools for XKit and your dashboard, such as increased font sizes, more contrast on icons and more. **//
 //* DEVELOPER new-xkit **//
