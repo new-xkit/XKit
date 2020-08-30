8b0f370631b01910ff387002473a518699ce3747
Marcus <marcustyphoon@gmail.com>
2020-08-30 02:54:20 -0700

    changing this label for consistency

--- Extensions/show_originals.js
+++ Extensions/show_originals.js
@@ -358,7 +358,7 @@ XKit.extensions.show_originals = new Object({
 				const reblogicon = '<svg viewBox="0 0 12.3 13.7" width="24" height="14" fill="var(--blog-contrasting-title-color,var(--transparent-white-65))" fill-opacity="0.75"><path d="M9.2.2C8.7-.2 8 .2 8 .8v1.1H3.1c-2 0-3.1 1-3.1 2.6v1.9c0 .5.4.9.9.9.1 0 .2 0 .3-.1.3-.1.6-.5.6-.8V5.2c0-1.4.3-1.5 1.3-1.5H8v1.1c0 .6.7 1 1.2.6l3.1-2.6L9.2.2zM12 7.4c0-.5-.4-.9-.9-.9s-.9.4-.9.9v1.2c0 1.4-.3 1.5-1.3 1.5H4.3V9c0-.6-.7-.9-1.2-.5L0 11l3.1 2.6c.5.4 1.2.1 1.2-.5v-1.2h4.6c2 0 3.1-1 3.1-2.6V7.4z"></path></svg>';
 				const note_text = `${blogName} ${reblogicon} ${rebloggedFromName}`;
 				const aria_label = `hidden post: ${blogName} reblogged a post from ${rebloggedFromName}`;
-				const button = '<div class="xkit-button showoriginals-hidden-button">show reblog</div>';
+				const button = '<div class="xkit-button showoriginals-hidden-button">show post</div>';
 
 				$this.prepend(`<div class="showoriginals-hidden-note" aria-label="${aria_label}">${note_text}${button}</div>`);
 				$this.on('click', '.showoriginals-hidden-button', XKit.extensions.show_originals.unhide_post);
