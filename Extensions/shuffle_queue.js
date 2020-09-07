0f993b8aa5cfa9b390f015983378ed4043533249
Marcus <marcustyphoon@gmail.com>
2020-09-07 11:26:44 -0700

    shrink posts: cleaner jquery maipulation

--- Extensions/shuffle_queue.js
+++ Extensions/shuffle_queue.js
@@ -375,16 +375,14 @@ XKit.extensions.shuffle_queue = new Object({
 			const $post = $(this).addClass('queue_plus_shrink-done').find("article").first();
 			const $header = $post.find("header").first();
 
-			const outerHtml = `<div class="queue_plus_shrink_container"><div class="queue_plus_shrink_container_shadow"></div></div>.`;
-			const innerHtml = `<div class="queue_plus_shrink_container_inner"></div>`;
+			$header.next().css("margin", 0);
 
-			const $shrinkContainer = $(outerHtml).insertAfter($header);
-			const $shrinkContainerInner = $(innerHtml).appendTo($shrinkContainer);
-
-			$shrinkContainer.next().css("margin", 0);
-
-			//move everything between the header and footer into the shrink container
-			$shrinkContainer.nextUntil("footer").appendTo($shrinkContainerInner);
+			$header.nextUntil("footer")
+			.wrapAll(
+				`<div class="queue_plus_shrink_container">
+					<div class="queue_plus_shrink_container_inner"></div>
+				</div>`)
+			.parent().before('<div class="queue_plus_shrink_container_shadow"></div>');
 		});
 	},
 
