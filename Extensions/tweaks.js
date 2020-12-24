a13cceaa4ca4b74ff8ab96cae37d6511c7d1bfeb
motackt <42635772+motackt@users.noreply.github.com>
2020-12-25 00:02:11 +1100

    Version bump

--- Extensions/tweaks.js
+++ Extensions/tweaks.js
@@ -1,5 +1,5 @@
 //* TITLE Tweaks **//
-//* VERSION 6.0.11 **/
+//* VERSION 6.0.12 **/
 //* DESCRIPTION Various little tweaks for your dashboard. **//
 //* DEVELOPER new-xkit **//
 //* DETAILS These are small little tweaks that allows you customize your dashboard. If you have used XKit 6, you will notice that some of the extensions have been moved here as options you can toggle. Keep in mind that some of the tweaks (the ones marked with a '*') can slow down your computer. **//
