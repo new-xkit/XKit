c15b71c4e1e198377e5c23802f2c9eb8e2807724
Marcus <marcustyphoon@gmail.com>
2022-06-21 10:30:31 -0700

    Update legacy url for async_form_key

--- Extensions/xkit_patches.js
+++ Extensions/xkit_patches.js
@@ -1180,7 +1180,7 @@ XKit.extensions.xkit_patches = new Object({
 			};
 
 			XKit.interface.async_form_key = async function() {
-				const request = await fetch('https://www.tumblr.com/about');
+				const request = await fetch('https://www.tumblr.com/developers');
 				const meta_tag = (await request.text()).match(
 					/tumblr-form-key[^>]*content=("([^"]+)"|'([^']+)')/
 				);
