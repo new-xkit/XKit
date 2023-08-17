1f256034f0e579808abf508687377eb2d1e96884
Amari Low <sceptile_rayquaza@hotmail.com>
2023-08-17 17:08:56 +1000

    Fix formatting issues identified by CI Test
    Replaces space indents with tabs, and removes spaces before function parantheses

--- Extensions/denotifier.js
+++ Extensions/denotifier.js
@@ -6,18 +6,17 @@
 //* BETA false **//
 XKit.extensions.denotifier = new Object({
 
-    running: false,
+	running: false,
 
-    run: async function () {
-        this.running = true;
-        await XKit.css_map.getCssMap();
-        const { navItem, notificationBadgeIn } = XKit.css_map.cssMap;
-        XKit.tools.add_css(`.${navItem}:not([title='Messages']) .${notificationBadgeIn} { display: none !important; }`, "denotifier");
-
+	run: async function() {
+		this.running = true;
+		await XKit.css_map.getCssMap();
+		const { navItem, notificationBadgeIn } = XKit.css_map.cssMap;
+		XKit.tools.add_css(`.${navItem}:not([title='Messages']) .${notificationBadgeIn} { display: none !important; }`, "denotifier");
     },
 
-    destroy: function () {
-        this.running = false;
-        XKit.tools.remove_css("denotifier");
-    }
+	destroy: function() {
+		this.running = false;
+		XKit.tools.remove_css("denotifier");
+	}
 });
