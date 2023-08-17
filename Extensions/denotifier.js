b997dde425ae57497b90ccc680cd3d4292a7cea1
Amari Low <sceptile_rayquaza@hotmail.com>
2023-08-17 16:57:53 +1000

    Create denotifier.js
    Initial commit for new De-Notifier extension.

---
+++ Extensions/denotifier.js
@@ -0,0 +1,23 @@
+//* TITLE De-Notifier **//
+//* VERSION 1.0.0 **//
+//* DESCRIPTION Removes non-essential unread notification badges. **//
+//* DEVELOPER circlejourney **//
+//* FRAME false **//
+//* BETA false **//
+XKit.extensions.denotifier = new Object({
+
+    running: false,
+
+    run: async function () {
+        this.running = true;
+        await XKit.css_map.getCssMap();
+        const { navItem, notificationBadgeIn } = XKit.css_map.cssMap;
+        XKit.tools.add_css(`.${navItem}:not([title='Messages']) .${notificationBadgeIn} { display: none !important; }`, "denotifier");
+
+    },
+
+    destroy: function () {
+        this.running = false;
+        XKit.tools.remove_css("denotifier");
+    }
+});
