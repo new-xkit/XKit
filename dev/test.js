b4f102bf7fec654f49e920bc3849d903be90a38b
Marcus <marcustyphoon@gmail.com>
2024-11-22 10:46:56 -0800

    Revert "create test script"
    This reverts commit 8c07e4cc159302951679428bcb20f45584f57013.

--- dev/test.js
+++
@@ -1,9 +0,0 @@
-const globby = require('globby');
-
-const files = globby
-.sync('{Extensions/dist/*.json,Extensions/dist/page/gallery.json,Extensions/dist/page/list.json,Extensions/dist/page/themes.json}', {
-	cwd: '.',
-	dot: false,
-});
-
-console.log(files);
