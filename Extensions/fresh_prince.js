cfddc1551d7b108c9741633683f06135431a5285
Marcus <marcustyphoon@gmail.com>
2024-12-02 03:00:17 -0800

    remove lodash dependency from fresh prince

--- Extensions/fresh_prince.js
+++ Extensions/fresh_prince.js
@@ -1,5 +1,5 @@
 //* TITLE Fresh Prince **//
-//* VERSION 0.1.1 **//
+//* VERSION 0.1.2 **//
 //* DESCRIPTION Everything's flipped, turned upside down **//
 //* DEVELOPER new-xkit **//
 //* DETAILS This extension flips everything on the dashboard. Don't expect Tumblr to function correctly whatsoever while this extension is installed. **//
@@ -76,7 +76,6 @@ XKit.extensions.fresh_prince = new Object({
 
 		if (!XKit.extensions.fresh_prince.preferences.disable_rotate.value) {
 			await XKit.css_map.getCssMap();
-			const {keyToCss, descendantSelector} = XKit.css_map;
 
 			XKit.tools.add_css(`
 				.l-header-container, .l-container, .l-footer-container {
@@ -90,11 +89,11 @@ XKit.extensions.fresh_prince = new Object({
 					transform: scale(1, -1);
 				}
 
-				${descendantSelector('menuContainer', 'baseContainer')} {
+				${XKit.css_map.descendantSelector('menuContainer', 'baseContainer')} {
 					transform: scale(1, -1);
 				}
 
-				${keyToCss('popoverChrome')} > * {
+				${XKit.css_map.keyToCss('popoverChrome')} > * {
 					transform: scale(1, -1);
 				}
 
